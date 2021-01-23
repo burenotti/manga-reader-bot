49a025312306ed016d7f50f1bb308bb60d45e4e2
Артём Буренин <burenotti@gmail.com>
2021-01-23 22:15:39 +0300

    telegram and telegraph tokens added as environment variables

--- environment.py
+++ environment.py
@@ -2,3 +2,5 @@ import os
 
 ROOT_DIR = os.path.dirname(__file__)
 SELENIUM_WEBDRIVER_DIR = os.path.abspath(os.environ.get('WEBDRIVERDIR'))
+TELEGRAM_API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
+TELEGRAPH_API_TOKEN = os.environ.get('TELEGRAPH_API_TOKEN')
